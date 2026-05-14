2eaa08301adedd02f74d9dfa4fcfa0f88f778725
TGVERSION <yaroslavtsev6464@mail.ru>
2026-05-14 22:11:39 +0500

    feat: add GitHub Pages deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/sp12_site',
+  images: { unoptimized: true },
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
